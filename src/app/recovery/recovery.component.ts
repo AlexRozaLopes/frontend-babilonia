42c18b3dc93d18386d3161279efd49c4397d8dfb
Alex Roza Lopes <alexrozaestudos@gmail.com>
2025-06-09 17:25:07 -0300

    add funcionalidades ao fluxo de reset de senha

--- src/app/recovery/recovery.component.ts
+++ src/app/recovery/recovery.component.ts
@@ -1,19 +1,49 @@
-import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { Component, inject, Injectable } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
+  standalone: true, // ✅ importante para usar `imports`
   selector: 'app-recovery',
   templateUrl: './recovery.component.html',
   imports: [
+    MatSnackBarModule,
     FormsModule
   ]
 })
+@Injectable({ providedIn: 'root' })
 export class RecoveryComponent {
+  private http = inject(HttpClient);
+  private snackBar = inject(MatSnackBar);
+
   email = '';
 
   onSubmit() {
     console.log('Solicitar recuperação para:', this.email);
-    alert('Se este email estiver cadastrado, você receberá um link para redefinir sua senha.');
-    // Aqui você implementa a lógica de envio de email
+
+    const payload = {
+      email: this.email
+    };
+
+    this.http.patch('/api/user/change-password', payload, {
+      responseType: 'text'
+    }).subscribe({
+      next: () => {
+        this.snackBar.open(
+          'Se este email estiver cadastrado, um link de recuperação foi enviado!',
+          '',
+          { duration: 5000, panelClass: ['snackbar-success'] }
+        );
+      },
+      error: (err) => {
+        console.error(err);
+        this.snackBar.open(
+          'Erro ao tentar enviar o e-mail. Tente novamente mais tarde.',
+          '',
+          { duration: 5000, panelClass: ['snackbar-error'] }
+        );
+      }
+    });
   }
 }
